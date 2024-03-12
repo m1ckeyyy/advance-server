1a8613b22b5dbdb24ea36a57f396384e28ad229e
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2024-03-12 19:03:20 +0100

    adjustments

--- server.js
+++ server.js
@@ -2,10 +2,27 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Offer = require("./offerSchema");
-// const Offer = require("./offerSchema");
+const cors = require("cors");
 
 require("dotenv").config();
 const uri = process.env.MONGO_URI;
+
+app.use(express.json());
+app.use(
+	cors({
+		origin: "http://127.0.0.1:3000", //https://qhc5nx-5173.preview.csb.app
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	})
+);
+// app.use((req, res, next) => {
+// 	res.set("Access-Control-Allow-Origin", "http://127.0.0.1:3000"); //https://qhc5nx-5173.preview.csb.app
+// 	// res.set('Access-Control-Allow-Origin', 'http://localhost:3000'); //https://qhc5nx-5173.preview.csb.app
+// 	res.setHeader("Access-Control-Allow-Credentials", true);
+// 	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+// 	res.setHeader("Access-Control-Allow-Headers", "Authorization");
+// 	next();
+// });
 
 //DB
 
@@ -18,7 +35,42 @@ mongoose
 app.get("/", (req, res) => {
 	res.send("Welcome");
 });
+app.post("/admin", (req, res) => {
+	try {
+		let { email, password } = req.body;
+		email = email.toLowerCase();
+		console.log(req.body);
+		User.findOne({ email }).then((user) => {
+			if (!user) {
+				return res.status(404).send("Invalid email or password");
+			}
+			bcrypt.compare(password, user.password).then((isMatching) => {
+				if (isMatching) {
+					// password = user.password;
+					console.log("matching!");
+					const accessToken = jwt.sign(
+						email,
+						process.env.ACCESS_TOKEN_SECRET
+					);
+					res.cookie("access_token", accessToken, {
+						httpOnly: false,
+						secure: false,
+					});
+					res.status(200).send({ message: "Admin verified" });
+				} else {
+					console.log("not matching :(");
+					res.status(401).send({
+						message: "Incorrect email or password",
+					});
+				}
+			});
+		});
+	} catch (e) {
+		res.status(500).send({ message: "An error occured" });
+	}
+	res.status(200).send("COOKIE TOKEN: xaxaxaxaxaxa");
+});
 
 app.listen(4000, () => {
-	console.log("Connected to port 4000", "");
+	console.log("Connected to port 4000");
 });
